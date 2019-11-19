82a787d9abae89b7f9565020c3f4d89be13c266b
Mafe Morris <mafemorris99@gmail.com>
2019-11-19 15:10:38 -0500

    con friccion

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -17,9 +17,14 @@ void escribe(string archivo, T* datos, int n_dat){
   //cierra el archivo.
   outfile.close(); 
 }
-
+//sin friccion
 void rk4 (double* v, double* x, double k, double m, double lambda, double h, int pasos);
+//con friccion
+void rk4 (double* v, double* x, double k, double m, double lambda, double h, double gamma, int pasos);
 void euler(double* x, double* v, double h, double k, double m, double lambda, int pasos);
+//con friccion
+double dx(double x, double v, double gamma, double k, double m, double lambda);
+//sin friccion
 double dx(double x, double k, double m, double lambda);
 double dv(double v);
 
@@ -28,7 +33,7 @@ int main(){
     double k = 100;
     double m = 2;
     double h = 0.01;
-    double gamma = 2;
+    double gamma = 5;
     double x0 = 0;
     double xf = 10;
     int pasos = (xf-x0)/h;
@@ -49,8 +54,18 @@ int main(){
     escribe("xrk4.txt",xrk4,pasos);
     escribe("vrk4.txt",vrk4,pasos);
 
+    double* xrk4f = new double[pasos];
+    double* vrk4f = new double[pasos];
+
+    rk4(vrk4f,xrk4f,k,m,lambda,h,gamma,pasos);
+
+    escribe("xrk4f.txt",xrk4f,pasos);
+    escribe("vrk4f.txt",vrk4f,pasos);
+
     delete[] xrk4;
     delete[] vrk4;
+    delete[] xrk4f;
+    delete[] vrk4f;
     delete[] x;
     delete[] v;
 
@@ -59,7 +74,9 @@ int main(){
 double dx(double x, double k, double m, double lambda){
     return (-k/m)*pow(x,lambda);
 }
-
+double dx(double x, double v, double gamma, double k, double m, double lambda){
+    return (-k/m)*pow(x,lambda) - gamma*v;
+}
 double dv(double v){
     return v;
 }
@@ -71,7 +88,6 @@ void euler(double* x, double* v, double h, double k, double m, double lambda, in
     for(int i=1; i<pasos; i++){
         x[i] = x[i-1] + h*dx(x[i-1],k,m,lambda); 
         v[i] = v[i-1] + h*dv(v[i-1]);
-        cout << x[i] << " " << v[i] << endl;
     }
 }
 
@@ -96,6 +112,29 @@ void rk4 (double* v, double* x, double k, double m, double lambda, double h, int
 
         x[i] = x[i-1] + (1.0/6.0)*(k10 + 2*k20 + 2*k30 + k40);
         v[i] = v[i-1] + (1.0/6.0)*(k11 + 2*k21 + 2*k31 + k41);
-        cout << v[i] << "\t" << x[i] << endl;
+    }
+}
+
+void rk4 (double* v, double* x, double k, double m, double lambda, double h, double gamma, int pasos){
+    x[0] = 1; //condiciones iniciales
+    v[0] = 1;
+    for(int i = 1; i< pasos; i++){
+        double k10, k11, k20, k21, k30, k31, k40, k41;
+    
+        k10 = h*dv(v[i-1]);
+        k11 = h*dx(x[i-1], v[i-1], gamma, k, m, lambda);
+        
+        k20 = h*dv(v[i-1] + k10/2);
+        k21 = h*dx(x[i-1] + k11/2, v[i-1] + k10/2, gamma, k, m, lambda);
+
+        k30 = h*dv(v[i-1] + k20/2);
+        k31 = h*dx(x[i-1] + k21/2, v[i-1] + k20/2, gamma, k, m, lambda);
+
+        k40 = h*dv(v[i-1] + k30);
+        k41 = h*dx(x[i-1] + k31, v[i-1] + k30, gamma, k, m, lambda);
+        
+
+        x[i] = x[i-1] + (1.0/6.0)*(k10 + 2*k20 + 2*k30 + k40);
+        v[i] = v[i-1] + (1.0/6.0)*(k11 + 2*k21 + 2*k31 + k41);
     }
 }
--- ejercicio.py
+++ ejercicio.py
@@ -3,6 +3,8 @@ import matplotlib.pylab as plt
 
 xrk4 = np.loadtxt("xrk4.txt")
 vrk4 = np.loadtxt("vrk4.txt")
+xrk4f = np.loadtxt("xrk4f.txt")
+vrk4f = np.loadtxt("vrk4f.txt")
 x = np.loadtxt("x.txt")
 v = np.loadtxt("v.txt")
 
@@ -13,6 +15,10 @@ plt.savefig("ondita.png")
 plt.figure()
 plt.plot(xrk4,vrk4)
 plt.savefig("faserk4.png")
+
+plt.figure()
+plt.plot(xrk4f,vrk4f)
+plt.savefig("faserk4f.png")
 
 plt.figure()
 plt.plot(x,v)
